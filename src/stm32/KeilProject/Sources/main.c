e2afe540b27cf323b7d962e1ff2f11ac6cdba4bf
ClementGermain <clement.germain.22@gmail.com>
2016-11-08 12:10:34 +0100

    Move stm32 files.c into the sources's folder of the keil project

--- src/stm32/KeilProject/Sources/main.c
+++ src/stm32/KeilProject/Sources/main.c
@@ -1,37 +1,51 @@
-#include "Board_LED.h"
-#include "stm32f10x.h"                  // Device header
-#include "stm32f10x_gpio.h"
+
 #include "stm32f10x.h"
+#include "gpio.h"
+#include "pwm.h"
+#include "motor.h"
 
-int main()
+// Uncomment the line below according to the target STM32 device used
+
+//#define STM32F10X_LD 					// STM32F10X_LD: STM32 Low density devices
+//#define STM32F10X_LD_VL				// STM32F10X_LD_VL: STM32 Low density Value Line devices 
+//#define STM32F10X_MD 					// STM32F10X_MD: STM32 Medium density devices
+//#define STM32F10X_MD_VL 			// STM32F10X_MD_VL: STM32 Medium density Value Line devices 
+//#define STM32F10X_HD 					// STM32F10X_HD: STM32 High density devices
+//#define STM32F10X_HD_VL 			// STM32F10X_HD_VL: STM32 High density value line devices 
+//#define STM32F10X_XL					// STM32F10X_XL: STM32 XL-density devices
+//#define STM32F10X_CL					// STM32F10X_CL: STM32 Connectivity line devices
+ 
+void RCC_Configuration(void);
+ 
+int main(void)
 {
+	// RCC Configuration
+  RCC_Configuration();
 	
-	//LED_Initialize();
-	RCC_DeInit();
+	Init_All_Motor();
 	
-	GPIO_InitTypeDef Init;
-	Init.GPIO_Pin = GPIO_Pin_4;
-	Init.GPIO_Speed = GPIO_Speed_50MHz;
-	Init.GPIO_Mode = GPIO_Mode_Out_PP;
+	Turn_Left(1.0);
+	Go_Forward(1.0);
 	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-	//GPIO_AFIODeInit();
-	GPIO_Init(GPIOA, &Init);
-	GPIO_SetBits(GPIOA, GPIO_Pin_4);
-	
-	
-	while (1)
-	{
-		//LED_On(0);
-
-		//sleep(1);
-		for (int i=0 ; i<5000000 ; i++);
-		
-		
-		//LED_Off(0);
-		//sleep(1);
-		for (int i=0 ; i<5000000 ; i++);
-	}
-	
-	return 0;
+  while (1)
+  {}
+}
+ 
+/**
+  * @brief  Configures the different system clocks.
+  * @param  None
+  * @retval None
+  */
+void RCC_Configuration(void)
+{
+  /* PCLK1 = HCLK/4 */
+  RCC_PCLK1Config(RCC_HCLK_Div4);
+ 
+  /* TIM clock enable */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2 | RCC_APB1Periph_TIM3 | RCC_APB1Periph_TIM4, ENABLE);
+ 
+  /* GPIO clock enable */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOA |
+                         RCC_APB2Periph_AFIO, ENABLE);
 }
