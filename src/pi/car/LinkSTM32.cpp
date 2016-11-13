60fc7b7560333a8887a3e3190d7130213651f1cf
LaJack <Lucas.Jacques469@gmail.com>
2016-11-13 22:46:49 +0100

    Set up SPI comminucation raspi's side

--- src/pi/car/LinkSTM32.cpp
+++ src/pi/car/LinkSTM32.cpp
@@ -1,6 +1,8 @@
 #include <thread>
+#include <cstring>
 #include "Car.hpp"
 #include "LinkSTM32.hpp"
+#include "../SPI_Interface/SPI_Interface.hpp"
 
 using namespace std;
 
@@ -8,9 +10,10 @@ using namespace std;
 LinkSTM32::LinkSTM32(int period_ms) :
 	period_ms(period_ms),
 	endThread(false),
+	SPI{SPI_CHANNEL, SPI_SPEED},
 	threadCom([this] {this->run(); })
 {
-
+	SPI.Start();
 }
 
 LinkSTM32::~LinkSTM32() {
@@ -22,19 +25,27 @@ void LinkSTM32::run() {
 	BarstowControl_Typedef control;
 	BarstowModel_Typedef model;
 
+	const int bufferSize =	max(sizeof(BarstowControl_Typedef),
+								sizeof(BarstowModel_Typedef))
+							/ sizeof(unsigned char)
+							+ sizeof(unsigned char);
+
+	unsigned char spiBuffer[bufferSize];
+
 	while(!endThread) {
 		// Fetch model structure from STM32 via SPI
-		// TODO write in 'model'
 		
+		// Get 'control' from Car
+		Car::getControlStructure(control);
+		
+		// Exchange data with SPI
+		std::memcpy(spiBuffer, &control, sizeof(BarstowControl_Typedef));
+		SPI.Send(spiBuffer, bufferSize)
+		std::memcpy(&model, spiBuffer, sizeof(BarstowModel_Typedef));
+
 		// Update Car
 		Car::updateModelStructure(model);
 
-		// Get 'control' from Car
-		Car::getControlStructure(control);
-
-		// Send control structure to STM32 via SPI
-		// TODO read 'control'
-		
 		// Wait a bit then repeat
 		this_thread::sleep_for(chrono::milliseconds(period_ms));
 	}
--- src/pi/car/LinkSTM32.hpp
+++ src/pi/car/LinkSTM32.hpp
@@ -3,6 +3,9 @@
 
 #include <thread>
 
+#define SPI_SPEED 500000
+#define SPI_CHANNEL 0
+
 class LinkSTM32 {
 	public:
 		LinkSTM32(int period_ms=50);
@@ -13,6 +16,7 @@ class LinkSTM32 {
 
 		const int period_ms;
 		bool endThread;
+		SPI_Interface SPI;
 		std::thread threadCom;
 };
 
