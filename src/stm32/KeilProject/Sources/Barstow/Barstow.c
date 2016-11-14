5d5d560163d13a773541e87f4da5d7af45b40f9a
ClementGermain <clement.germain.22@gmail.com>
2016-11-14 18:42:09 +0100

    correct some incorrect prototype functions

--- src/stm32/KeilProject/Sources/Barstow/Barstow.c
+++ src/stm32/KeilProject/Sources/Barstow/Barstow.c
@@ -40,6 +40,4 @@ void StartBarstow(void)
 		Update_Motors(BarstowControl);
 		for (int i=0 ; i < 50000 ; i++);
 	}
-	
-	return;
 }
--- src/stm32/KeilProject/Sources/SPI_Interface/SPI_Interface.h
+++ src/stm32/KeilProject/Sources/SPI_Interface/SPI_Interface.h
@@ -1,6 +1,6 @@
 #ifndef __SPI_INTERFACE_H
 #define __SPI_INTERFACE_H
 
-void InitializeSPI2(void);
+void InitializeSPI2(unsigned char * receiveBuffer, int receiveLen, unsigned char * sendBuffer, int sendLen);
 
 #endif
