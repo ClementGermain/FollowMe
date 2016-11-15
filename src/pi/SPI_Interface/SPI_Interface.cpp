a6730746593032abab4a6026578b3c22d42b81d6
Pro1d <topAffaire_pg@hotmail.fr>
2016-11-15 11:42:18 +0100

    Handle correctly "noraspi" target in SPI_interface

--- src/pi/SPI_Interface/SPI_Interface.cpp
+++ src/pi/SPI_Interface/SPI_Interface.cpp
@@ -1,5 +1,7 @@
 #include "SPI_Interface.hpp" 
+#ifndef __NO_RASPI__
 #include <wiringPiSPI.h>
+#endif
 
 using namespace std;
 
@@ -16,11 +18,19 @@ SPI_Interface::~SPI_Interface()
 
 int SPI_Interface::Start()
 {
+#ifndef __NO_RASPI__
 	return  wiringPiSPISetup (m_channel, m_speed);
+#else
+	return 0;
+#endif
 }
 
 int SPI_Interface::Send(unsigned char* data, int len)
 {
+#ifndef __NO_RASPI__
 	int n = wiringPiSPIDataRW (m_channel, data, len) ;
 	return n;
+#else
+	return 0;
+#endif
 }
