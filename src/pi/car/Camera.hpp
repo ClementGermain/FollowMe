26e75fd5593a65cdfe0905537cf4f813d8c23526
Pro1d <topAffaire_pg@hotmail.fr>
2016-11-16 13:54:17 +0100

    Create static instance of Camera, add improc/user

--- src/pi/car/Camera.hpp
+++ src/pi/car/Camera.hpp
@@ -16,29 +16,22 @@
 
 class Camera {
 	public:
-		Camera(const char * winName, int width=DEFAULT_FRAME_WIDTH, int height=DEFAULT_FRAME_HEIGHT, int framerate=DEFAULT_FRAMERATE);
+		Camera(int width=DEFAULT_FRAME_WIDTH, int height=DEFAULT_FRAME_HEIGHT, int framerate=DEFAULT_FRAMERATE);
 		~Camera();
-		// Open camera preview window
-		void openPreview();
-		// Close camera preview window
-		void closePreview();
 		// Give image from camera
 		void getImage(cv::Mat & out);
 		SDL_Surface * getBitmap(double scale);
 
 	private:
-		void updatePreview();
-		static void loopPreview(Camera * that);
 
-		const std::string windowName;
 		cv::Mat imageCam;
 #ifndef __NO_RASPI__
 		RaspiCamCvCapture * raspiCam;
 		RASPIVID_CONFIG configCam;
 #endif
-		std::thread * threadPreview;
-		bool threadRunning;
 };
+
+extern Camera RaspiCam;
 
 #endif
 
