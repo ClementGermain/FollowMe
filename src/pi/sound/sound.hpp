4fa296cfaaca05bd72c1ca5f0b361159edbf2e33
Raspberry Pi <raspberry@pi.com>
2016-11-17 19:06:50 +0100

    add two sound options (play /stop) in the UI

---
+++ src/pi/sound/sound.hpp
@@ -0,0 +1,16 @@
+#ifndef __SOUND__
+#define __SOUND__
+
+/*
+class Sound {
+	public:
+		Sound(void);
+		static void play_test(void);
+	private:
+};
+*/
+
+void play_test(void);
+
+void stop_sound(void);
+#endif
