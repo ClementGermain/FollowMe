7f15a948a82a82a25fcfc9f65abf46f447673fdc
Pro1d <topaffaire_pg@hotmail.fr>
2016-11-20 10:59:35 +0100

    Camera is now fully static

--- src/pi/car/Camera.cpp
+++ src/pi/car/Camera.cpp
@@ -1,7 +1,9 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
+#include <chrono>
 #include <thread>
 #include <unistd.h>
+#include <cmath>
 #include "Camera.hpp"
 
 #ifndef __NO_RASPI__
@@ -11,49 +13,128 @@
 using namespace std;
 
 
-Camera RaspiCam;
+const float Camera::height			= 1.0f;
+const float Camera::pitch			= 20 * M_PI/180;
+const float Camera::horizontalFOV	= 53 * M_PI/180;
+const float Camera::verticalFOV		= 41 * M_PI/180;
+std::mutex Camera::camLock;
+IplImage * Camera::imageCam;
+std::chrono::time_point Camera::lastCaptureDate;
+#ifndef __NO_RASPI__
+RaspiCamCvCapture * Camera::raspiCam = NULL;
+RASPIVID_CONFIG Camera::configCam;
+#endif
 
-Camera::Camera(int width, int height, int framerate)
+void Camera::init(int width, int height, int framerate)
 {
 #ifndef __NO_RASPI__
-	// Init configuration
-	configCam.width = width;
-	configCam.height = height;
-	configCam.bitrate = 0; // default
-	configCam.framerate = framerate;
-	configCam.monochrome = 0; // default
-	
-	// Initialize the camera
-	raspiCam = raspiCamCvCreateCameraCapture2(0, &configCam);
+	if(raspiCam == NULL) {
+		// Init configuration
+		configCam.width = width;
+		configCam.height = height;
+		configCam.bitrate = 0; // default
+		configCam.framerate = framerate;
+		configCam.monochrome = 0; // default
+
+		// Initialize the camera
+		raspiCam = raspiCamCvCreateCameraCapture2(0, &configCam);
+	}
 #endif
 }
 
-Camera::~Camera() {
+void Camera::destroy() {
 #ifndef __NO_RASPI__
-	// Release camera
-	raspiCamCvReleaseCapture(&raspiCam);
+	if(raspiCam != NULL) {
+		// Release camera
+		raspiCamCvReleaseCapture(&raspiCam);
+	}
 #endif
+	if(imageCam != NULL) {
+		cvReleaseImage(&imageCam);
+	}
+}
+
+int Camera::getFrameWidth() {
+	return configCam.width;
+}
+
+int Camera::getFrameHeight() {
+	return configCam.height;
+}
+
+int Camera::getFrameRate() {
+	return configCam.framerate;
+}
+
+int Camera::getFrameDuration() {
+	return 1000 / configCam.framerate + 1;
+}
+
+void Camera::updateImage() {
+	// Get elapsed time since last capture
+	chrono::duration<chrono::milliseconds> timeSinceLastCapture = chrono::system_clock::now() - lastCaptureDate;
+	
+	// Update the capture if too old
+	if(timeSinceLastCapture > getFrameDuration() || imageCam == NULL) {
+		// lock the mutex 
+		// FIXME->it can block the current thread and all the others waiting threads for 30ms
+		camLock.lock();
+
+		// release the previous image
+		if(imageCam != NULL)
+			cvRealeaseImage(&imageCam);
+		
+		// If the pending frame is too old, skip it
+		if(timeSinceLastCapture > 2*getFrameDuration())
+			raspiCamCvQueryFrame(raspiCam);
+		
+		// get the next frame (can wait for 1/framerate second max...)
+		imageCam = cvCloneImage(raspiCamCvQueryFrame(raspiCam));
+		// update the capture date
+		lastCaptureDate = chrono::system_clock::now();
+		
+		// unlock the mutex
+		camLock.unlock();
+	}
 }
 
 void Camera::getImage(cv::Mat & out) {
+	updateImage();
+
 #ifndef __NO_RASPI__
-	out = raspiCamCvQueryFrame(raspiCam);
+	// lock the mutex 
+	camLock.lock();
+
+	// Create a new matrix from the current capture
+	out = imageCam;
+
+	// unlock the mutex
+	camLock.unlock();
 #endif
 }
 
-SDL_Surface * Camera::getBitmap(double scale) {
+SDL_Surface * Camera::getBitmap() {
+	updateImage();
+
 #ifndef __NO_RASPI__
-	IplImage * img = raspiCamCvQueryFrame(raspiCam);
-	//cv::resize(img, img, cv::Size(0,0), scale, scale);
-	return SDL_CreateRGBSurfaceFrom((void*)img->imageData,
+	// lock the mutex 
+	camLock.lock();
+
+	// Create a new SDL_SUrface from the current capture
+	s = SDL_CreateRGBSurfaceFrom((void*)img->imageData,
 			img->width,
 			img->height,
 			img->depth * img->nChannels,
 			img->widthStep,
 			0xff0000, 0x00ff00, 0x0000ff, 0
 	);
+
+	// unlock the mutex
+	camLock.unlock();
 #else
-	return SDL_CreateRGBSurface(SDL_SWSURFACE, DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT, 32, 0,0,0,0);
+	s = SDL_CreateRGBSurface(SDL_SWSURFACE, DEFAULT_FRAME_WIDTH, DEFAULT_FRAME_HEIGHT, 32, 0,0,0,0);
 #endif
+
+	 return s;
 }
 
--- src/pi/car/Camera.hpp
+++ src/pi/car/Camera.hpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <SDL/SDL.h>
+#include <chrono>
 
 #ifndef __NO_RASPI__
 	#include "RaspiCamCV.h"
@@ -16,22 +17,44 @@
 
 class Camera {
 	public:
-		Camera(int width=DEFAULT_FRAME_WIDTH, int height=DEFAULT_FRAME_HEIGHT, int framerate=DEFAULT_FRAMERATE);
-		~Camera();
+		// Initialize the camera
+		static void init(int width=DEFAULT_FRAME_WIDTH, int height=DEFAULT_FRAME_HEIGHT, int framerate=DEFAULT_FRAMERATE);
+		// Release the camera
+		static void destroy();
+		// update the current frame
+		static void updateImage();
 		// Give image from camera
-		void getImage(cv::Mat & out);
-		SDL_Surface * getBitmap(double scale);
+		static void getImage(cv::Mat & out);
+		static SDL_Surface * getBitmap();
+		// Dynamic properties
+		static int getFrameWidth(); // in Pixels
+		static int getFrameHeight(); // in Pixels
+		static int getFrameRate(); // in Hertz
+		static int getFrameDuration(); // in Milliseconds
 
+		/// Static camera's position and properties
+		// Distance of the cam above ground (meter)
+		static const float height;
+		// Distance from the center of the car (horizontal 'roll' axis) (meter)
+		static const float distanceToCenter;
+		// pitch angle from horizontal (radians)
+		static const float pitch;
+		// horizontal field of view of the camera sensor (radians)
+		static const float horizontalFOV;
+		// vertical field of view of the camera sensor (radians)
+		static const float verticalFOV;
+		
 	private:
-
-		cv::Mat imageCam;
+		
+		static std::mutex camLock;
+		static IplImage * imageCam;
+		static std::chrono::time_point lastCaptureDate;
 #ifndef __NO_RASPI__
-		RaspiCamCvCapture * raspiCam;
-		RASPIVID_CONFIG configCam;
+		static RaspiCamCvCapture * raspiCam;
+		static RASPIVID_CONFIG configCam;
 #endif
 };
 
-extern Camera RaspiCam;
 
 #endif
 
