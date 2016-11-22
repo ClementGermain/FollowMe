592237f94923f3bbd57744edf85ad1726f63a008
Florian Viguier <flowiguier@gmail.com>
2016-11-22 12:58:36 +0100

    modif ussensor.c

--- src/stm32/KeilProject/Sources/interface/us_sensor.c
+++ src/stm32/KeilProject/Sources/interface/us_sensor.c
@@ -16,7 +16,7 @@ US_Sensor_Typedef * SENSOR_FRONT_C 	= &SENSOR_FRONT_C_;
 US_Sensor_Typedef * SENSOR_BACK_L 	= &SENSOR_BACK_L_;
 US_Sensor_Typedef * SENSOR_BACK_R 	= &SENSOR_BACK_R_;
 US_Sensor_Typedef * SENSOR_BACK_C 	= &SENSOR_BACK_C_;
-US_Sensor_Typedef * US_Active;
+US_Sensor_Typedef * US_active;
 
 //Global time (ms)
 int Time;
@@ -35,16 +35,16 @@ void Update_US_Sensor(BarstowModel_Typedef * Modele){
 // configure the pins TRIGG and ECHO in output and input  
 void Init_US_Sensor(US_Sensor_Typedef * Sensor){
 	if (Sensor == SENSOR_FRONT_L){
-			Init_GPIO_In(SENSOR_FRONT_L->GPIO_Echo, SENSOR_FRONT_L->GPIO_Pin_Echo);
-			//Init_GPIO_Out(SENSOR_FRONT_L->GPIO_Trig, SENSOR_FRONT_L->GPIO_Pin_Trig);
+		//Init_GPIO_In(SENSOR_FRONT_L->GPIO_Echo, SENSOR_FRONT_L->GPIO_Pin_Echo);
+		Init_GPIO_Out(SENSOR_FRONT_L->GPIO_Trig, SENSOR_FRONT_L->GPIO_Pin_Trig);
 	}
 	else if (Sensor ==  SENSOR_FRONT_R){
-		Init_GPIO_In(SENSOR_FRONT_R->GPIO_Echo, SENSOR_FRONT_R->GPIO_Pin_Echo);
-		//Init_GPIO_Out(SENSOR_FRONT_R->GPIO_Trig, SENSOR_FRONT_R->GPIO_Pin_Trig);
+		//Init_GPIO_In(SENSOR_FRONT_R->GPIO_Echo, SENSOR_FRONT_R->GPIO_Pin_Echo);
+		Init_GPIO_Out(SENSOR_FRONT_R->GPIO_Trig, SENSOR_FRONT_R->GPIO_Pin_Trig);
 	}
 	else if (Sensor == SENSOR_FRONT_C){
-		Init_GPIO_In(SENSOR_FRONT_C->GPIO_Echo, SENSOR_FRONT_C->GPIO_Pin_Echo);
-		//Init_GPIO_Out(SENSOR_FRONT_C->GPIO_Trig, SENSOR_FRONT_C->GPIO_Pin_Trig);	
+		//Init_GPIO_In(SENSOR_FRONT_C->GPIO_Echo, SENSOR_FRONT_C->GPIO_Pin_Echo);
+		Init_GPIO_Out(SENSOR_FRONT_C->GPIO_Trig, SENSOR_FRONT_C->GPIO_Pin_Trig);	
 	}
 	else if (Sensor == SENSOR_BACK_L){
 		Init_GPIO_In(SENSOR_BACK_L->GPIO_Echo, SENSOR_BACK_L->GPIO_Pin_Echo);
