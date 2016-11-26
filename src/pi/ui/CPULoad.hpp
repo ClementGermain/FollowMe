08c57288579f4319c4d63477b013d194bd71a9c7
Pro1d <topAffaire_pg@hotmail.fr>
2016-11-26 02:01:04 +0100

    Comment and improve CPU load measure

--- src/pi/ui/CPULoad.hpp
+++ src/pi/ui/CPULoad.hpp
@@ -1,17 +1,18 @@
 #ifndef __CPU_LOAD_HPP__
 #define __CPU_LOAD_HPP__
 
-#include <SDL/SDL.h>
 #include <unistd.h>
+#include "utils/Timer.hpp"
 
 class CPULoad {
 	public:
 		CPULoad();
+		/** Return the cpu load in percentage, updated no more than once per second **/
 		float get();
 	private:
 		size_t prevIdleTime;
 		size_t prevTotalTime;
-		Uint32 prevDate;
+		Timer timer;
 		float prevLoad;
 };
 
