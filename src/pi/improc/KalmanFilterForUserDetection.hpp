60d036be6bdd2d212d61c5f6e2a6a43e479d1934
Noemie972 <noastro@gmail.com>
2016-12-08 15:10:55 +0100

    Kalman filter compilatio ok

--- src/pi/improc/KalmanFilterForUserDetection.hpp
+++ src/pi/improc/KalmanFilterForUserDetection.hpp
@@ -2,17 +2,15 @@
 #define __KALMAN_FILTER_FOR_USER_DETECTION_HPP__
 
 #include <opencv2/opencv.hpp>
-#include "UserPatternDetection.hpp"
 #include <vector>
 #include <stdio.h>
 
 class Kalman_Filter_User{
-public:
-			static int x; //lateral position of the user
-			static int y; // longitudinal position
-			static int r; // radius 
-
-			Kalman_Filter_User() ; //constructor to initialize 
-			Kalman_Filter_User_Detection();
-
+	public:
+		Kalman_Filter_User() ; //constructor to initialize 
+		cv::Mat Kalman_Filter_User_Detection(float x, float y, float r);
+	private:
+		cv::KalmanFilter K_Filter;
 };
+
+#endif
