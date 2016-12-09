abb572948339fb18e0008c51f99cab8a34fb4da3
ClementGermain <clement.germain.22@gmail.com>
2016-12-09 14:11:24 +0100

    add a motor model class

---
+++ src/pi/car/MotorModel.cpp
@@ -0,0 +1,60 @@
+#include "MotorModel.hpp"
+#include <cmath>
+#include <unistd.h>
+#include <iostream>
+#include <fstream>
+#include "../../stm32/KeilProject/Sources/Barstow/Model.h"
+
+MotorModel::MotorModel(void){
+  MotorModel(600);
+}
+
+MotorModel::MotorModel(int sizeModel_) : sizeModel (sizeModel_) {
+  StateMotor_TypeDef model [sizeModel];
+
+  // init the state model
+  for (int i=0 ; i<sizeModel ; i++){
+    model[i].cmd = 0;
+    model[i].current = 0;
+    model[i].voltage1 = 0;
+    model[i].voltage2 = 0;
+    model[i].speed = 0;
+  }
+}
+  
+void MotorModel::create(int CmdStart, int CmdStop, float waitTime){
+  
+  BarstowModel_Typedef BarstowModel;
+  int i=0;
+
+  for (float cmd=CmdStart ; cmd<CmdStop ; cmd+=(CmdStop-CmdStart)/sizeModel){
+    Car::writeControlMotor(Car::MoveForward, cmd);
+    
+    sleep(waitTime);
+
+    Car::updateModelStructure(BarstowModel);
+    model[i].current = cmd;
+    model[i].current = BarstowModel.leftWheelMotor.current;
+    model[i].current = BarstowModel.leftWheelMotor.voltage1;
+    model[i].current = BarstowModel.leftWheelMotor.voltage2;
+    model[i].current = BarstowModel.leftWheelMotor.speed; 
+    
+    i++;
+  }  
+}
+
+void MotorModel::save(const std::string & fileName){
+  ofstream file(fileName, ios::out | ios::trunc);
+if (file){
+  fwrite(&model, sizeof(StateMotor_TypeDef), sizeModel, file);
+  file.close();
+  }
+}
+
+void MotorModel::load(const std::string & fileName){
+  ifstream file(fileName); 
+  if (file){
+    fread(&model, sizeof(StateMotor_TypeDef), sizeModel, file);
+    file.close();
+  }
+}
---
+++ src/pi/car/MotorModel.hpp
@@ -0,0 +1,40 @@
+#ifndef __MOTORMODEL__
+#define __MOTORMODEL__
+
+#include <string>
+#include <cstdio>
+#include "Car.hpp"
+
+// struct that will contain all the models motors informations
+typedef struct {
+  uint32_t cmd;
+  uint32_t current;
+  uint32_t voltage1;
+  uint32_t voltage2;
+  uint32_t speed;
+} StateMotor_TypeDef;
+
+class MotorModel {
+
+public :
+  MotorModel(void);
+  MotorModel(int sizeModel);
+
+  // Create the model by running a simulation and get motor's value
+  // time in seconds
+  void create(int cmdStart, int CmdStop, float waitTime);
+
+  // save the model into a file
+  void save(const std::string & FileName);
+
+  // load the model
+  void load(const std::string & FileName);
+
+  StateMotor_TypeDef getState(uint32_t cmd);
+
+protected :
+  int sizeModel;
+  StateMotor_TypeDef *  model;
+};
+
+#endif
