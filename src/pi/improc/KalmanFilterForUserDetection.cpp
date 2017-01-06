835805c6dc2408a0e11c1b994675b81617e901a0
student <student@romeo1204x32.(none)>
2017-01-06 16:38:04 +0100

    Fix kalman filter

--- src/pi/improc/KalmanFilterForUserDetection.cpp
+++ src/pi/improc/KalmanFilterForUserDetection.cpp
@@ -12,55 +12,92 @@ using namespace std;
 
 
 
-Kalman_Filter_User::Kalman_Filter_User() : K_Filter(3,3,0){
+Kalman_Filter_User::Kalman_Filter_User() : K_Filter(3,3,0)
+{
+	resetState(0,0,0);
 
-	//init
-	K_Filter.statePre.at<float>(0) = 0;
-	K_Filter.statePre.at<float>(1) = 0;
-	K_Filter.statePre.at<float>(2) = 0;
-	K_Filter.transitionMatrix = *(Mat_<float>(3, 3) << 1,0,0,   0,1,0,  0,0,1); //A
-	setIdentity(K_Filter.measurementMatrix);// H = I
+	// A, should take in account speed and acceleration
+	setIdentity(K_Filter.transitionMatrix);	// = *(Mat_<float>(3, 3) << 1,0,0,   0,1,0,  0,0,1); // A
+	// H
+	setIdentity(K_Filter.measurementMatrix);
 	
 	//parameters:
-	float halfWidth = Camera::getFrameWidth() * 0.5f;
-	float focalLength = halfWidth / tan(Camera::horizontalFOV/2);
-	float v_max_user=1.38; //m per seconds; (5km/h)
-	float time = (float)UserPattern::frameDurationMillis / 1000; //seconds
-	float d=1.80;
-	float R_cible= UserPattern::CircleRadius;
-	float delta_x_max = focalLength / d * (v_max_user * time); 
-	float delta_r_max = fabs(focalLength * R_cible/(d-v_max_user*time) - focalLength * R_cible/d); 
-
-	LogI << "d max: "<< (v_max_user*time)<<" Delta X max: "<<delta_x_max << "   Delta R max: "<<delta_r_max << endl;
-
-	setIdentity(K_Filter.processNoiseCov, Scalar::all(0.0));//Q 
-	K_Filter.processNoiseCov.at<float>(1,1) = delta_x_max;
-	K_Filter.processNoiseCov.at<float>(2,2) = 5;
-	K_Filter.processNoiseCov.at<float>(3,3) = delta_r_max;
-		
-	//setIdentity(K_Filter.measurementNoiseCov, Scalar::all(1.0));  //R
-	//K_Filter.measurementNoiseCov.at<float>(2,2) = 5.0;
-	K_Filter.measurementNoiseCov = *(Mat_<float>(3, 3) << 	10,0,0,   0,10,0,  0,0,65); //TAB EXCEL
-	setIdentity(K_Filter.errorCovPost, Scalar::all(1));//P
-
-	
+	float d = 0.80f;// default distance
+	// Q
+	updateProcessNoise(d);
+	// R
+	updateMeasurementNoise(d);
+	// P
+	setIdentity(K_Filter.errorCovPost, Scalar::all(1));
 }
 
+void Kalman_Filter_User::updateMeasurementNoise(float userDistance) {
+	// select radius variance from table according to the current distance
+	const int nbValues = 2;
+	float distances[nbValues] = {0.8, 1.8};
+	float variances[nbValues] = {31, 65};
+	float var;
+	if(userDistance <= distances[0])
+		var = variances[0];
+	else if(userDistance >= distances[nbValues-1])
+		var = variances[nbValues-1];
+	else {
+		int i = 1;
+		while(userDistance > distances[i])
+			i++;
+		float k = (userDistance - distances[i-1]) / (distances[i] - distances[i-1]);
+		var = k * variances[i-1] + (1-k) * variances[i];
+	}
 
-Mat Kalman_Filter_User::Kalman_Filter_User_Detection(float x, float y, float r){
+	LogI << "Radius Variance: " << var << " for distance: " << userDistance << endl;
 
-	// First predict, to update the internal statePre variable
-	Mat prediction = K_Filter.predict(); //compute a predicted state
+	K_Filter.measurementNoiseCov = *(Mat_<float>(3, 3) << 	10,0,0,   0,10,0,  0,0,var); // TAB EXCEL
+}
+
+void Kalman_Filter_User::resetState(float x, float y, float r) {
+	K_Filter.statePre.at<float>(0) = x;
+	K_Filter.statePre.at<float>(1) = y;
+	K_Filter.statePre.at<float>(2) = r;
+}
+
+void Kalman_Filter_User::updateProcessNoise(float userDistance) {
+	// Parameters for calculations
+	float halfWidth = Camera::getFrameWidth() * 0.5f;
+	float focalLength = halfWidth / tan(Camera::horizontalFOV/2);
+
+	float time = (float) UserPattern::frameDurationMillis / 1000; //seconds
+	float v_max_user = 1.38f; // m/s (=> 5km/h)
+	float moving = time * v_max_user;
+	
+	float R_cible = UserPattern::CircleRadius;
+	
+	// Maximum x and radius change between 2 frames
+	float delta_x_max = focalLength / userDistance * moving;
+	float delta_r_max = (userDistance - moving > 0.8f) ? focalLength * R_cible * (1.0f / (userDistance - moving) - 1.0f / userDistance) : 1; 
+
+	LogI << "d max: "<< moving << " Delta X max: " << delta_x_max << "   Delta R max: " << delta_r_max << endl;
+
+	setIdentity(K_Filter.processNoiseCov, Scalar::all(0.0)); // Q 
+	K_Filter.processNoiseCov.at<float>(0,0) = delta_x_max;
+	K_Filter.processNoiseCov.at<float>(1,1) = Camera::getFrameHeight() * 0.02;
+	K_Filter.processNoiseCov.at<float>(2,2) = delta_r_max;
+}
+
+Mat Kalman_Filter_User::correctMeasurement(float x, float y, float r){
+	Mat prediction = K_Filter.predict();
+
 	//Get position x, y and radius r
 	Mat measurement = Mat::zeros(3,1, CV_32F);
 	measurement.at<float>(0) = x;
 	measurement.at<float>(1) = y;
 	measurement.at<float>(2) = r;
+	
 	//The "correct" phase that is going to  use the predicted value and our measurement
 	return K_Filter.correct(measurement);
-
 }
 
-
-
+Mat Kalman_Filter_User::predictNoMeasurement() {
+	//compute a predicted state
+	return K_Filter.predict();
+}
 
--- src/pi/improc/KalmanFilterForUserDetection.hpp
+++ src/pi/improc/KalmanFilterForUserDetection.hpp
@@ -8,7 +8,11 @@
 class Kalman_Filter_User{
 	public:
 		Kalman_Filter_User() ; //constructor to initialize 
-		cv::Mat Kalman_Filter_User_Detection(float x, float y, float r);
+		cv::Mat correctMeasurement(float x, float y, float r);
+		cv::Mat predictNoMeasurement();
+		void updateProcessNoise(float userDistance);
+		void updateMeasurementNoise(float userDistance);
+		void resetState(float x, float y, float r);
 		//void Mesure_parameters_calcul_KF(float xm, float ym, float rm, float x_mes[1000], float y_mes[1000], float r_mes[1000]);
 		
 	private:
